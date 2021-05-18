7e2cfe0002e327fc5d8d78f4d225136d91a200ee
Ubuntu <ubuntu@ip-172-31-14-26.ec2.internal>
2021-05-18 16:11:29 +0000

    fixed changes

--- volume_of_tetrahedron.py
+++ volume_of_tetrahedron.py
@@ -4,17 +4,19 @@
 # Created on: May 2021
 # Python program to calculate volume of a tetrahedron
 # create a function that calculates the volume
+
+
 import math
 
 
-# Function that calculates the volume of the tetrahedron
 def main():
+    # Function that calculates the volume of the tetrahedron
     # User input
     edge = int(input("Enter the value (mm): "))
     # Process
     volume = (edge ** 3) / (6 * math.sqrt(2))
     # Output
-    print("This is the volume: {}mm³".format(volume))
+    print("This is the volume: {} mm³".format(volume))
 
 
 if __name__ == "__main__":
